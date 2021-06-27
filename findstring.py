daab49e2336be7071cee12bc1f01d1d303364157
syamuna <85882227+syamuna@users.noreply.github.com>
2021-06-27 16:30:39 +0530

    Create findstring.py

---
+++ findstring.py
@@ -0,0 +1,8 @@
+def count_substring(string, sub_string):
+    ml=len(string)
+    sl=len(sub_string)
+    c=0
+    for i in range(ml-sl+1):
+        if(string[i:(i+sl)]==sub_string):
+            c=c+1
+    return c
